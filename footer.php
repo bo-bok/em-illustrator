57caa3ff177136e6ad847c890cd83df0c0050b36
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-29 15:37:02 +0100

    adds static footer code to footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
-</p>footer</p>
+<!--  PAGE FOOTER
+  ============================================================= -->
+  <div class="row footer">
+    <div class="col-12 footer-text">copyright Emily Mcgovern. Made with love and coffee by Jessica Salmon</div>
+  </div>
+
+</div>
+</body>
+
+</html>
