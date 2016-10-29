04c2ce2d2d7d229c8663ff9451738b714afc4400
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-29 15:23:51 +0100

    rename frot-page.php so that it no longer overrides index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<!-- The main template. If your Theme provides its own templates, index.php must be present. -->
+
 <?php get_header(); ?>
 
 <h1>Index file</h1>
 
 <?php get_footer(); ?>
-<!-- The main template. If your Theme provides its own templates, index.php must be present. -->
