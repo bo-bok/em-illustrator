02bdd392e4a790c35502d5ffd0459b49b6be6269
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-29 15:53:48 +0100

    misc

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
 <!--  PAGE FOOTER
   ============================================================= -->
-  <div class="row footer">
+  <footer class="row footer">
     <div class="col-12 footer-text">
-      Copyright Emily McGovern <?php echo date('Y')?>. Made with love and coffee by Jessica Salmon
+      Copyright Emily McGovern <?php echo date('Y'); ?>. Made with love and coffee by Jessica Salmon
     </div>
-  </div>
+
+    <?php wp_footer(); ?>
+    <!-- you can output any scripts here, e.g. js -->
+
+  </footer>
 
 </div>
 </body>
