bf26edda2c289fc8d2ecc5e49097721928bbe467
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-30 19:41:53 +0000

    add code from wpt to page.php for static pages

--- page.php
+++ page.php
@@ -1,1 +1,24 @@
 <!-- the page template. used when an individual page is queried. -->
+
+<?php get_header(); ?>
+
+<section class="row">
+  <div class="small-12 columns text-center">
+    <div class="leader">
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
+
+      <?php endwhile; else : ?>
+
+        <p><?php _e( 'Sorry, no posts matched your criteria.', 'treehouse-portfolio' ); ?></p>
+
+      <?php endif; ?>
+
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
