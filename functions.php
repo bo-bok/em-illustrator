fc9b36b42250bce81070227ce747043710fa805a
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-30 22:03:45 +0000

    add menu support

--- functions.php
+++ functions.php
@@ -7,6 +7,22 @@
 
 <?php
 
+// these functions add theme support for x
+add_theme_support( 'menus' );
+// add_theme_support( 'post-thumbnails' );
+
+
+// this function makes WP recognise our menu
+function register_theme_menus() {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_theme_menus' );
+
+
  // this function loads the style (css) files
  function em_theme_styles() {
    wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
