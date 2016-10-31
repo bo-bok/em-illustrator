c6820d4db16e580709d0582bd88ca816a7ead7cc
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-31 09:06:24 +0000

    adds home page static code to front-page.php

--- front-page.php
+++ front-page.php
@@ -1,1 +1,39 @@
 <!-- The front page template. -->
+<!-- see fton-page.php if php loop is needed -->
+
+<?php get_header(); ?>
+
+
+<!-- gallery grid system  -->
+<div class="row">
+  <!-- <div class="col-1"></div> -->
+  <div class="col-12 col-bkg">
+
+    <div class="table-container">
+
+      <div class="table-row">
+        <div class="table-cell right-align">
+          <a href=""><img class="homepage-image" src="assets/homepage-images/1-slytherin-600x600px.png" /></a>
+        </div>
+        <div class="table-cell left-align">
+          <a href=""><img class="homepage-image" src="assets/homepage-images/2-published-work-600x600px.png" /></a>
+        </div>
+      </div>
+
+      <div class="table-row">
+        <div class="table-cell right-align">
+          <a href=""><img class="homepage-image" src="assets/homepage-images/3-cards-comics-600x600px.png" /></a>
+        </div>
+        <div class="table-cell left-align">
+          <a href=""><img class="homepage-image" src="assets/homepage-images/3-cards-comics-600x600px.png" /></a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!-- <div class="col-1"></div> -->
+</div>
+
+
+<?php get_footer(); ?>
