461a21cac4bcd8373099e782406b8952905a78a8
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-08 12:09:52 +0000

    adds images to homepage. solution: get_template_directory_uri()

--- front-page.php
+++ front-page.php
@@ -1,65 +1,36 @@
-<!-- The front page template. -->
-<!-- see fton-page.php if php loop is needed -->
-
 <?php get_header(); ?>
-
 
 <!-- gallery grid system  -->
 <div class="row">
   <div class="col-12 col-bkg">
 
-    <div class="table-container">
+    <div class="table-container-1">
+    <div class="table">
 
       <div class="table-row">
-        <div class="table-cell right-align">
-          <a href=""><img class="homepage-image" src="assets/homepage-images/1-slytherin-600x600px.png" /></a>
+        <div class="table-cell">
+          <a href=""><img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/1-slytherin-600x600px.png" /></a>
         </div>
-        <div class="table-cell left-align">
-          <a href=""><img class="homepage-image" src="assets/homepage-images/2-published-work-600x600px.png" /></a>
+
+        <div class="table-cell">
+          <a href=""><img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/2-published-work-600x600px.png" /></a>
         </div>
       </div>
 
       <div class="table-row">
-        <div class="table-cell right-align">
-          <a href=""><img class="homepage-image" src="assets/homepage-images/3-cards-comics-600x600px.png" /></a>
+        <div class="table-cell">
+          <a href=""><img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/3-cards-comics-600x600px.png" /></a>
         </div>
-        <div class="table-cell left-align">
-          <a href=""><img class="homepage-image" src="assets/homepage-images/3-cards-comics-600x600px.png" /></a>
+        <div class="table-cell">
+          <a href=""><img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/3-cards-comics-600x600px.png" /></a>
         </div>
       </div>
 
     </div>
+  </div>
 
   </div>
 </div>
 
-<!-- gallery grid system  -->
-<!-- <div class="row">
-  <div class="col-12 col-bkg">
-
-    <div class="table-container">
-
-      <div class="table-row">
-        <div class="table-cell right-align">
-          <a href="<?php the_permalink(); ?>"><img class="homepage-image" src="<?php the_post_thumbnail(); ?>" /></a>
-        </div>
-        <div class="table-cell left-align">
-          <a href="<?php the_permalink(); ?>"><img class="homepage-image" src="<?php the_post_thumbnail(); ?>" /></a>
-        </div>
-      </div>
-
-      <div class="table-row">
-        <div class="table-cell right-align">
-          <a href="<?php the_permalink(); ?>"><img class="homepage-image" src="<?php the_post_thumbnail(); ?>" /></a>
-        </div>
-        <div class="table-cell left-align">
-          <a href="<?php the_permalink(); ?>"><img class="homepage-image" src="<?php the_post_thumbnail(); ?>" /></a>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-</div> -->
-
+<!-- use plugin to add image to category - wp term images -->
 <?php get_footer(); ?>
