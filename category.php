c94d21eae78025c41ea2c442a942b76e06ae1e6a
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-08 12:47:54 +0000

    #8

--- category.php
+++ category.php
@@ -3,16 +3,14 @@
 get_header(); ?>
 
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+// WP Loop to display content, if there is any.
+?>
 
   <h1><?php the_title(); ?></h1>
   <p><?php the_content(); ?></p>
 
-<?php endwhile; else : ?>
-
-  <p><?php _e( 'Sorry, no posts matched your criteria.', 'treehouse-portfolio' ); ?></p>
-
-<?php endif; ?>
+<?php endwhile; endif; ?>
 
 
 
