ff0d0d7ad6d88b6a2175e957a4f4df679bb2f076
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-10 13:51:46 +0000

    category & content-portfolio.php

--- category.php
+++ category.php
@@ -4,15 +4,21 @@
 
 get_header(); ?>
 
+<?php echo category_description(); ?>
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
     // WP Loop to display content, if there is any.
   ?>
 
     <h1><?php the_title(); ?></h1>
+    <hr />
+
     <p><?php the_content(); ?></p>
 
   <?php endwhile; endif; ?>
 
+
+
+  <!-- <?php get_template_part('content', 'portfolio'); ?> -->
 
 <?php get_footer(); ?>
