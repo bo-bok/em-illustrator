2ab2e1fe2e336d73c2080052388f4688da16b2f0
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-10 15:30:09 +0000

    attempted functions for displayed attachments by category in category.php

--- category.php
+++ category.php
@@ -6,32 +6,6 @@ get_header(); ?>
 
 <?php echo category_description(); ?>
 
-<?php
-  function search_filter($query) {
-  if ( !is_admin() && $query->is_main_query() ) {
-    if ($query->is_category) {
-      $query->set('post_type', array( 'post', 'attachment' ) );
-    }
-  }
-}
-add_action('pre_get_posts','search_filter');
-
-?>
-
-
-<?php get_footer(); ?>
-
-
-
-
-
-
-
-
-
-
-
-
 <!-- <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
   // WP Loop to display content, if there is any.
 ?>
@@ -42,3 +16,6 @@ add_action('pre_get_posts','search_filter');
   <p><?php the_content(); ?></p>
 
 <?php endwhile; endif; ?> -->
+
+
+<?php get_footer(); ?>
