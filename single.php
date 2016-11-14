b72b79a53506e092bc024593a1e547e9b5f7fa3d
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-14 11:31:59 +0000

    #18 create single.php template that shows individual images

---
+++ single.php
@@ -0,0 +1,28 @@
+<?php
+
+// The main template. If your Theme provides its own templates, index.php must be present.
+
+get_header(); ?>
+
+
+  <section class="row">
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <h1><?php the_title(); ?></h1>
+      <?php the_post_thumbnail( 'medium' ); ?>
+      <?php the_content(); ?>
+      <p>test single.php</p>
+
+      <li class="cat">in <?php the_category( ' ' ); ?></li>
+      <li class="date ">on <?php the_time( 'F j, Y' ); ?></li>
+
+
+      <div class="alignleft"><?php next_post_link( ); ?></div>
+      <div class="alignright"><?php previous_post_link( ); ?></div>
+
+    <?php endwhile; endif; ?>
+
+  </section>
+
+<?php get_footer(); ?>
