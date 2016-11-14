0d0c638d3e425127efa5c55dff1d7c7889a9b2f7
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-14 14:04:10 +0000

    clean up category.php

--- category.php
+++ category.php
@@ -4,38 +4,25 @@
 
 get_header(); ?>
 
-<?php echo category_description(); ?>
+  <?php echo category_description(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-  // WP Loop to display content, if there is any.
-?>
+  <div class="row thumbnail-category-container">
 
-  <!-- <h1><?php the_title(); ?></h1> -->
-<p>this is coming from category.php</p>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<div class="float-left">
+      <div class="category-page-thumbnail">
+        <a class="category-page-thumbnail-image" href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail( 'category-thumb'); ?>
+        </a>
+      </div>
 
-  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'category-thumb' ); ?></a>
+    <?php endwhile; endif; ?>
 
-</div>
-<!-- <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a> -->
+  </div>
 
-<!-- <div class="alignleft"><?php next_posts_link( 'Next post' ); ?></div>
-<div class="alignright"><?php previous_posts_link( 'Previous posts' ); ?></div> -->
-
-
-
-<?php endwhile; endif; ?>
-
+  <div class="row">
+    <p>this is coming from category.php</p>
+  </div>
 
 
 <?php get_footer(); ?>
-
-
-
- <!-- foreach ($terms as $key => $term) {
-    print "table cell";
-  if ($key % 2 === 1)
-    print "</div><div class="table-row">"
-  }
-?>  -->
