7ea7496441522c0fab83fdd9e1e0fc2dc45202da
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-15 12:25:46 +0000

    add single category image

--- single.php
+++ single.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
       <h1><?php the_title(); ?></h1>
-  <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
+      <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
       <?php the_content(); ?>
 
 
