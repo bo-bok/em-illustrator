5b9fb26fae5f6e5bcfe53aed0828e9ee91a42112
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-15 15:10:55 +0000

    comment with next responsive layout steps

--- single.php
+++ single.php
@@ -6,14 +6,25 @@ get_header(); ?>
 
 
   <section class="row">
-    <div class="image-container-single">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <h1><?php the_title(); ?></h1>
-      <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
-      <?php the_content(); ?>
+    <div class="table-container">
+      <div class="table">
+        <!-- large image -->
+        <div class="table-cell">
 
+          <div class="image-container-single">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+            <h1><?php the_title(); ?></h1>
+            <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
+            <?php the_content(); ?>
+
+            <?php endwhile; endif; ?>
+
+          </div>
+
+        </div>
+      </div>
     </div>
 
       <p>test single.php</p>
@@ -22,10 +33,15 @@ get_header(); ?>
       <li class="date ">on <?php the_time( 'F j, Y' ); ?></li>
 
 
-      <div class="alignleft"><?php next_post_link( ); ?></div>
-      <div class="alignright"><?php previous_post_link( ); ?></div>
+      <div class=""><?php next_post_link( ); ?></div>
+      <div class=""><?php previous_post_link( ); ?></div>
 
-    <?php endwhile; endif; ?>
+<!--  thoughts: table with 3 columns (one for prev link, middle for image, last for next link,)-->
+<!--  issue: mobile performance: would be better to have links underneath the image at left and right, for more space -->
+<!--  solution: use classes (display: none) & media queries to hide table cells on mobile. issue: overwriting classes which isn't
+        ideal for performance! -->
+
+        <!-- OR, can I solve this with 'responsive grid', i.e. col-lg col-sm col-md -->
   </section>
 
 <?php get_footer(); ?>
