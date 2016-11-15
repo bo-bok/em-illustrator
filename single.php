99bcce050116fb2e64752e067d0700ee21994f3a
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-15 11:38:59 +0000

    add custom image names & tags

--- single.php
+++ single.php
@@ -6,12 +6,16 @@ get_header(); ?>
 
 
   <section class="row">
-
+    <div class="image-container-single">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
       <h1><?php the_title(); ?></h1>
-      <?php the_post_thumbnail( 'single-image' ); ?>
+  <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
       <?php the_content(); ?>
+
+
+    </div>
+
       <p>test single.php</p>
 
       <li class="cat">in <?php the_category( ' ' ); ?></li>
@@ -22,7 +26,6 @@ get_header(); ?>
       <div class="alignright"><?php previous_post_link( ); ?></div>
 
     <?php endwhile; endif; ?>
-
   </section>
 
 <?php get_footer(); ?>
