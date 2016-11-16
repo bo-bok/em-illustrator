05f5709dd62de68e81dadfffc519ba16c1bc4600
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-16 22:34:57 +0000

    repsonsive tweaks

--- category.php
+++ category.php
@@ -4,38 +4,52 @@
 
 get_header(); ?>
 
-  <?php echo category_description(); ?>
+  <div class="row">
+    <div class="col-12">
 
-  <div class="table-container">
-    <div class="table-category">
-
-      <div class="table-row-category">
-
-        <!-- category thumbnails: 3 medium + screen , 2 small screen, 1 x-small screen -->
-        <div class="table-cell-category">
-
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-            <div class="category-page-thumbnail col-sm-6 col-md-4">
-              <a class="category-page-thumbnail-image" href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail( 'category-thumb' ); ?>
-              </a>
-            </div>
-
-          <?php endwhile; endif; ?>
-
-        </div>
-      </div>
+      <div class="category-description"><h1><?php echo category_description(); ?></h1></div>
 
     </div>
   </div>
 
-
-
-
   <div class="row">
-    <p>this is coming from category.php</p>
-  </div>
+    <div class="col-12">
+
+
+      <!-- display category images -->
+        <div class="table-container">
+          <div class="table-category">
+
+            <div class="table-row-category">
+
+              <!-- category thumbnails: 3 medium + screen , 2 small screen, 1 x-small screen -->
+              <div class="table-cell-category">
+
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                  <div class="category-page-thumbnail col-sm-6 col-md-4">
+                    <a class="category-page-thumbnail-image" href="<?php the_permalink(); ?>">
+                      <?php the_post_thumbnail( 'category-thumb' ); ?>
+                    </a>
+                  </div>
+
+                <?php endwhile; endif; ?>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+
+        <div class="row">
+          <p>this is coming from category.php</p>
+        </div>
+
+
+
+</div>
+</div>
 
 <!-- simple table like that of font-page.phpc -->
 <?php get_footer(); ?>
