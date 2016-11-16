dc102cff064e06345cf7d555f0d94468adb0d38c
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-16 18:52:31 +0000

    segment grid layout by md/lg to make grid responsive, places grid in different stylesheet for clarity

--- functions.php
+++ functions.php
@@ -42,6 +42,7 @@ add_action( 'init' , 'wptp_add_categories_to_attachments' );
 function em_theme_styles() {
   wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
   wp_enqueue_style( 'AdabiCondensedLight_font', '//db.onlinewebfonts.com/c/15898c1b4ff12cded84f74a011b62a91?family=Abadi+MT+Condensed+Light' );
+  wp_enqueue_style( 'responsiveGrid_css', get_template_directory_uri() . '/responsive-grid.css');
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'em_theme_styles' );
