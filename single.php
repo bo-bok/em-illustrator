c89aef984794fe3c939391fdbb7d32541c3e9d28
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-17 21:53:49 +0000

    add structure for single.php

--- single.php
+++ single.php
@@ -6,35 +6,52 @@ get_header(); ?>
 
 
   <section class="row">
+    <div class="test-container-1">
 
-    <div class="table-container">
-      <div class="table-single">
-        <!-- large image -->
-        <div class="table-cell-single">
+      <div class="single-prev-post col-md-2">
+        <div class=""><?php previous_post_link( ); ?></div>
+      </div>
 
-          <div class="image-container-single">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="single-main-image-container col-md-8">
+        <div class="table-container">
 
-            <!-- <h1><?php the_title(); ?></h1> -->
-            <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
-            <?php the_content(); ?>
 
-            <?php endwhile; endif; ?>
+          <div class="table-single">
+            <!-- large image -->
+            <div class="table-cell-single">
 
+              <div class="image-container-single">
+
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <!-- <h1><?php the_title(); ?></h1> -->
+                <div class="single-image"><?php the_post_thumbnail( 'single-image' ); ?></div>
+                <?php the_content(); ?>
+
+                <?php endwhile; endif; ?>
+
+              </div>
+
+            </div>
           </div>
-
         </div>
       </div>
+
+      <div class="single-next-post col-md-2">
+        <div class=""><?php next_post_link( ); ?></div>
+      </div>
+
     </div>
+  </section>
 
-      <p>test single.php</p>
+  <div class="row">
+    <div class="return-to-cat-page"
+      <div class="cat">back to <?php the_category( ' ' ); ?></div>
+      <!-- <li class="date ">on <?php the_time( 'F j, Y' ); ?></li> -->
+    </div>
+  </div>
 
-      <li class="cat">in <?php the_category( ' ' ); ?></li>
-      <li class="date ">on <?php the_time( 'F j, Y' ); ?></li>
 
-
-      <div class=""><?php next_post_link( ); ?></div>
-      <div class=""><?php previous_post_link( ); ?></div>
 
 <!--  thoughts: table with 3 columns (one for prev link, middle for image, last for next link,)-->
 <!--  issue: mobile performance: would be better to have links underneath the image at left and right, for more space -->
@@ -42,6 +59,5 @@ get_header(); ?>
         ideal for performance! -->
 
         <!-- OR, can I solve this with 'responsive grid', i.e. col-lg col-sm col-md -->
-  </section>
 
 <?php get_footer(); ?>
