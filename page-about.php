be23871725271abddfe8319ab61f5ddb695068fd
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-18 12:28:20 +0000

    updates image to new square one. This page displays all that it should, as it should

--- page-about.php
+++ page-about.php
@@ -8,46 +8,32 @@
 // the page template. used when an individual page is queried.
 get_header(); ?>
 
-<!--  PAGE BODY
-============================================================= -->
-    <!-- gallery grid system  -->
-    <div class="row">
-      <div class="col-md-6 col-bkg">
+  <div class="row">
 
-        <!-- <div class="table-container">
-          <div class="table"> -->
-
-            <!-- <div class="table-row"> -->
-              <?php query_posts('cat=10'); while (have_posts()) : the_post(); ?>
-
-
-              <div class="table-cell left-box-about-image">
-                <!-- <img class="about-portrait-image" src="<?= get_template_directory_uri()?>/assets/aboutpage-images/em-self-portrait.png" /> -->
-                <div class="about-portrait-image"><?php the_post_thumbnail( 'about-portrait-img' ); ?></div>
-              </div>
-
-            </div>
-
-<div class="col-md-6">
-              <div class="table-cell right-box-about-text">
-                <div class="about-text">
-                  <?php the_content(); ?>
-                </div>
-              </div>
-
-            </div>
-
-              <?php endwhile; ?>
-
-            	<?php wp_reset_query(); ?>
-
-            <!-- </div> -->
-
-          <!-- </div>
-        </div> -->
-
+    <div class="col-md-6">
+      <div class="left-box-about-image">
+        <img class="about-portrait-image" src="<?= get_template_directory_uri()?>/assets/aboutpage-images/em-self-portrait--750px.png" />
       </div>
     </div>
 
+    <div class="col-md-6">
+      <div class="table-cell right-box-about-text">
+        <div class="about-text">
+
+          <p>Emily is from Brussels and writes bios for herself in the third person cos it
+           looks way more professional. She went to London to do a foundation degree in
+           art &amp; design, instantly hated it, so decided to do a BA in Russian instead and
+           spent a year living in a commune in the middle of the Russian forest.</p>
+
+          <p>She graduated with a First from University College London in 2014, and now
+          works full-time in an office and draws in her spare time.</p>
+
+          <p>If you&#39;d like to buy a print or card, get in touch at: info@emilymcgovern.com</p>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
 
 <?php get_footer(); ?>
