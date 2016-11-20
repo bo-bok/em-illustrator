84bacb075a88fb1277eb1d86c6bbfc81ea03ff45
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-20 09:58:26 +0000

    cleanup functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,5 @@
 <?php
 
- // this file is for:
-  //  - enqueue theme stylesheets and scripts. see wp_enqueue_scripts.
-  //  - enable theme features such as sidebars, navigation menus, post thumbnails, post formats, custom headers, custom backgrounds and others.
-  //  - define functions used in several template files of your theme.
-  //  - set up an options menu, giving site owners options for colors, styles, and other aspects of your theme.
-
-
 // add theme support for menus:
 add_theme_support( 'menus' );
 
@@ -20,7 +13,7 @@ add_image_size( 'single-image', 700, 99999);
 add_image_size( 'about-portrait-img', 700, 700);
 
 
-// this function makes WP recognise our menu
+// this function makes WP recognise our menu(s)
 function register_theme_menus() {
   register_nav_menus(
     array(
@@ -60,11 +53,8 @@ function em_theme_styles() {
 add_action( 'wp_enqueue_scripts', 'em_theme_styles' );
 
 
-// this function loads the scripts (js), order is important
+// this function loads the scripts (js)
 function em_theme_js() {
   wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true) ;
-  // wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false) ;
-  // wp_enqueue_script( 'name_here', get_template_directory_uri() . '/path_to_file', array('of', 'dependencies'), 'set_version',  appear_in_footer_BOOLEAN);
 }
 add_action( 'wp_enqueue_scripts', 'em_theme_js' );
-// we're saying, wordpress,when its time for you to enqueue these scripts,enqueue them from here
