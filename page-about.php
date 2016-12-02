bb5799e9c4c000a60f74c7796823d91834fc975b
Jessica Salmon <jessicasalmon05@gmail.com>
2016-12-02 17:41:15 +0000

    spacing

--- page-about.php
+++ page-about.php
@@ -2,7 +2,7 @@
   /*
     Template Name: About Page
   */
-  
+
 // the page template. used when an individual page is queried.
 get_header(); ?>
 
@@ -20,7 +20,11 @@ get_header(); ?>
        spent a year living in a commune in the middle of the Russian forest.</p>
 
       <p>She graduated with a First from University College London in 2014, and now
-      works full-time in an office and draws in her spare time.</p>
+      works full-time in an office and draws in her spare time. </p>
+
+      <p>In 2016 Emily’s comic
+      <a href="/index.php?p=123">'Bloodlust &amp; Bonnets’</a> was shortlisted for the prestigious
+      Observer/Jonathan Cape Graphic Short Story Prize.</p>
 
       <p>If you&#39;d like to buy a print or card, get in touch at:
         <a href="mailto:info@emilymcgovern.com">info@emilymcgovern.com</a>
