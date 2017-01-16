1a03c87be781ac30aff3270b0ded7d22cdb4f488
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-16 22:20:24 +0000

    shop page setup

--- page-shop.php
+++ page-shop.php
@@ -6,4 +6,18 @@
 get_header(); ?>
 
 
+<div class="table">
+
+  <div class="table-row">
+    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum.</p>
+  </div>
+
+  <div class="table-row">
+    <div id="shop-1"></div>
+    <div id="shop-2"></div>
+  </div>
+
+  </div>
+</div>
+
 <?php get_footer(); ?>
