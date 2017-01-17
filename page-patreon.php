794006700a174bca83ab73f4f5adf1b97b944f74
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-17 18:02:55 +0000

    layout patreon page

--- page-patreon.php
+++ page-patreon.php
@@ -5,5 +5,16 @@
 
 get_header(); ?>
 
+<div class="patreon-description row">
+  <div class="col-xs-12">
+    Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.
+  </div>
+</div>
+
+<div class="bloodlust-comic-full row">
+  <div class="col-xs-12">
+    <img src="<?= get_template_directory_uri()?>/assets/bloodlust/bloodlustAndBonnets-full-1000px.jpg" />
+  </div>
+</div>
 
 <?php get_footer(); ?>
