4eb27eb7edd1719e38106664446f3a8e25726936
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-17 12:54:20 +0000

    complete page layout (bottom text section)

--- page-shop.php
+++ page-shop.php
@@ -14,23 +14,37 @@ get_header(); ?>
 <div class="row test-row">
   <div class="col-xs-12 col-md-6 test-col">
     <div class="shop-table">
+
       <div class="shop-table-row">
         <div class="shop-table-cell">
           <img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/4-illustrations-600x600px.png" />
-
         </div>
       </div>
+
+      <div class="shop-table-row">
+        <div class="shop-table-cell">
+          Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
+        </div>
+      </div>
+
     </div>
   </div>
 
   <div class="col-xs-12 col-md-6 test-col">
     <div class="shop-table">
+
       <div class="shop-table-row">
         <div class="shop-table-cell">
           <img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/homepage-images/3-cards-comics-600x600px.png" />
-
         </div>
       </div>
+
+      <div class="shop-table-row">
+        <div class="shop-table-cell">
+          Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
+        </div>
+      </div>
+
     </div>
 
   </div>
