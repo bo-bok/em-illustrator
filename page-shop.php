cbb1ac9d95935cb096b9c26958bf4eedcfb4923c
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-24 09:28:40 +0000

    updates class names

--- page-shop.php
+++ page-shop.php
@@ -21,16 +21,16 @@ get_header(); ?>
 
 <div class="row">
   <div class="col-xs-12 col-md-6">
-    <div class="shop-table">
+    <div class="shop-info-container">
 
-      <div class="shop-table-row">
-        <div class="shop-table-cell">
+      <div class="shop-info-row">
+        <div class="shop-info-cell">
           <img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/shop-page-images/shop-etsy.jpg" />
         </div>
       </div>
 
-      <div class="shop-table-row">
-        <div class="shop-table-cell">
+      <div class="shop-info-row">
+        <div class="shop-info-cell">
           Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
         </div>
       </div>
@@ -39,16 +39,16 @@ get_header(); ?>
   </div>
 
   <div class="col-xs-12 col-md-6">
-    <div class="shop-table">
+    <div class="shop-info-container">
 
-      <div class="shop-table-row">
-        <div class="shop-table-cell">
+      <div class="shop-info-row">
+        <div class="shop-info-cell">
           <img class="homepage-image" src="<?= get_template_directory_uri()?>/assets/shop-page-images/shop-soc6.jpg" />
         </div>
       </div>
 
-      <div class="shop-table-row">
-        <div class="shop-table-cell">
+      <div class="shop-info-row">
+        <div class="shop-info-cell">
           Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
         </div>
       </div>
