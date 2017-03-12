5637e2263f40b4893b5774928d43197735183ee0
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-12 15:10:49 +0000

    change file names from patreon to bloodlust-and-bonnets to reflect new page purpose
    relates #56

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function em_theme_styles() {
   wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
   wp_enqueue_style( 'responsiveGrid_css', get_template_directory_uri() . '/responsive-grid.css');
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'patreon_css', get_template_directory_uri() . '/page-patreon.css' );
+  wp_enqueue_style( 'bloodlust-and-bonnets_css', get_template_directory_uri() . '/page-bloodlust-and-bonnets.css' );
   wp_enqueue_style( 'shop_css', get_template_directory_uri() . '/shop.css' );
 }
 add_action( 'wp_enqueue_scripts', 'em_theme_styles' );
--- page-bloodlust-and-bonnets.php
+++ page-bloodlust-and-bonnets.php
@@ -5,13 +5,13 @@
 
 get_header(); ?>
 
-<div class="patreon-description row">
+<div class="BB-description row">
   <div class="col-xs-12">
     Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.
   </div>
 </div>
 
-<div class="bloodlust-comic-full row">
+<div class="BB-comic-full row">
   <div class="col-xs-12">
     <img src="<?= get_template_directory_uri()?>/assets/bloodlust/bloodlustAndBonnets-full-1000px.jpg" />
   </div>
